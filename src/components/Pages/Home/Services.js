ce9ed6eabfb705935e2fa1e8d35170ecdc62af7f
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-16 18:02:56 +0600

    Add card and info

---
+++ src/components/Pages/Home/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h3>OUR SERVICES</h3>
+            <h2>Services We Provide</h2>
+
+        </div>
+    );
+};
+
+export default Services;
