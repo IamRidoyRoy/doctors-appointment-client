c95c7a62a3f7ae41b3bd2d0dc195474e52a3e92d
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-16 22:34:24 +0600

    Add exceptional Hero Section

--- src/components/Pages/Home/Services.js
+++ src/components/Pages/Home/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluride from '../../../assets/images/fluoride.png';
 import cavity from '../../../assets/images/cavity.png';
 import whitening from '../../../assets/images/whitening.png';
+import treatment from '../../../assets/images/treatment.png';
 import Service from './Service';
 const Services = () => {
     const services = [
@@ -32,14 +33,26 @@ const Services = () => {
                 <h2 className='text-4xl'>Services We Provide</h2>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-12'>
                 {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
                     ></Service>)
-
                 }
+            </div>
+            {/* Exceptional Hero Section */}
+            <div>
+                <div class="hero min-h-screen mx-auto">
+                    <div class="hero-content flex-col lg:flex-row">
+                        <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                            <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                            <button class="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
