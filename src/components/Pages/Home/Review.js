e28a28dc8bfc08fd1d63b521deb3b3c39a21f997
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-17 14:06:11 +0600

    Add Testimonials with Avatar and fake data

---
+++ src/components/Pages/Home/Review.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Review = ({ reviews }) => {
+    const { name, location, img, review } = reviews
+    return (
+        <div>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <p>{review}</p>
+
+                    <div className='flex items-center my-3'>
+                        <div class="avatar">
+                            <div class="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                <img src={img} alt='' />
+                            </div>
+                        </div>
+                        <div className='mx-3'>
+                            <p class="card-title">{name}</p>
+                            <p>Location: {location}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
---
+++ src/components/Pages/Home/Testimonial.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Review from './Review';
+import people1 from '../../../assets/images/people1.png';
+import people2 from '../../../assets/images/people1.png';
+import people3 from '../../../assets/images/people1.png';
+import quote from '../../../assets/icons/quote.svg';
+const Testimonial = () => {
+    const reviewData = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people1
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people2
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people3
+        }
+    ]
+    return (
+        <section>
+            <div className='flex justify-between my-10'>
+                <div>
+                    <h4 className='text-xl text-primary font-sans font-bold'>Testimonial</h4>
+                    <h2 className='text-4xl'>What Our Patients Says</h2>
+                </div>
+                <div>
+                    <img className='w-24 lg:w-48' src={quote} alt="" />
+                </div>
+            </div>
+            <div className='grid grid-cols-1  lg:grid-cols-3'>
+                {
+                    reviewData.map(reviews =>
+                        <Review
+                            key={reviews._id}
+                            reviews={reviews}
+                        ></Review>
+                    )
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Testimonial;
